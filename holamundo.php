b283fa42d29bf2d918d87d7665dacfcb3beb826a
UnaiBarandica <unaibv03@gmail.com>
2024-10-02 10:47:45 +0200

    Primer commit

---
+++ holamundo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initialscale=1.0">
+ <title>Hola Mundo</title>
+</head>
+<body>
+    <h1>si funciona
+ <?php
+
+$a=20;
+$b=20;
+$c=$a+$b;
+echo $c;
+ echo "Hola Mundo";
+ ?></h1>
+</body>
+</html>
